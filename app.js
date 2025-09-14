372ede882e28dd196c80056e7361ed6b77ebaa05
Omarinho34 <amri.omar34@outlook.fr>
2025-09-15 00:16:19 +0200

    Commentaires

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
+// Importer les modules nécessaires
 const express = require('express');
 const pokemons = require('./mock_pokemon');
 const {success} = require('./helper');
 
+// Initialiser l'application Express
 const app = express();
 const PORT = 3000;
 
+
 app.get('/', (req, res) => res.send('Bienvenue sur l\'API Pokémon !'));
 
+// Endpoint pour récupérer un Pokémon par son ID
 app.get('/api/pokemons/:id', (req, res) => {
     const id = parseInt(req.params.id);
     const pokemon = pokemons.find(pokemon => pokemon.id === id)
@@ -14,10 +18,12 @@ app.get('/api/pokemons/:id', (req, res) => {
     res.json(success(message, pokemon));
 });
 
+// Endpoint pour récupérer tous les Pokémons
 app.get('/api/pokemons', (req, res) => {
     const nbPokemons = pokemons.length;
     const message = `Il y a ${nbPokemons} pokémons dans la base de données.`
     res.json(success(message, pokemons));
 });
 
+// Démarrer le serveur
 app.listen(PORT, () => console.log(`Le serveur est en cours d'exécution sur http://localhost:${PORT}`));
