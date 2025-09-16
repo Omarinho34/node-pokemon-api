4da95103b8194b6d16e60303b80b7399bcc6e443
Omarinho34 <amri.omar34@outlook.fr>
2025-09-17 01:00:00 +0200

    Ajout de la méthode CREATE & UPDATE

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 // Importer les modules nécessaires
 const express = require('express');
 const pokemons = require('./mock_pokemon');
-const {success} = require('./helper');
+const {success, getUniqueId} = require('./helper');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 // Initialiser l'application Express
 const app = express();
 const PORT = 3000;
+
+// // Middleware pour logger les requêtes
+// const logger = (req, res, next) => {
+//     console.log(`Requête reçue : ${req.method} ${req.url}`);
+//     next();
+// };
+
+// app.use(logger);
+
+// Middleware pour logger les requêtes avec morgan
+app
+    .use(morgan('dev'))
+    .use(bodyParser.json());
 
 
 app.get('/', (req, res) => res.send('Bienvenue sur l\'API Pokémon !'));
@@ -23,6 +38,25 @@ app.get('/api/pokemons', (req, res) => {
     const nbPokemons = pokemons.length;
     const message = `Il y a ${nbPokemons} pokémons dans la base de données.`
     res.json(success(message, pokemons));
+});
+
+// Endpoint pour créer un nouveau Pokémon
+app.post('/api/pokemons', (req, res) => {
+    const id = getUniqueId(pokemons);
+    const pokemonCreated = { ...req.body, ...{ id: id, created: new Date() } };
+    pokemons.push(pokemonCreated);
+    const message = `Le pokémon ${pokemonCreated.name} a bien été créé.`
+    res.json(success(message, pokemonCreated));
+})
+
+app.put('/api/pokemons/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const pokemonUpdated = { ...req.body, id: id };
+    pokemons = pokemons.map(pokemon => {
+        return pokemon.id === id ? pokemonUpdated : pokemon;
+    });
+    const message = `Le pokémon ${pokemonUpdated.name} a bien été modifié.`
+    res.json(success(message, pokemonUpdated));
 });
 
 // Démarrer le serveur
